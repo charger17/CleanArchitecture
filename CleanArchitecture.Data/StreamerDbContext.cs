da0f9cb0d5247ec4d5767208a858567c07498cda
Carlos Perez Salazar <cps172779@gmail.com>
2023-07-05 13:40:01 -0600

    Add connection string

---
+++ CleanArchitecture.Data/StreamerDbContext.cs
@@ -0,0 +1,18 @@
+﻿using CleanArchitecture.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchitecture.Data
+{
+    public class StreamerDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.;Database=MagicVilla;Trusted_Connection=true;MultipleActiveResultSets=true;Integrated Security= true;TrustServerCertificate=True;");
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        public DbSet<Streamer>? Streamers { get; set; }
+        public DbSet<Video>? Videos { get; set; }
+
+    }
+}
