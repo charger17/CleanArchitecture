9cf5af9b83425804cdbaa932c44ade4c2f5e0a1f
Carlos Perez Salazar <cps172779@gmail.com>
2023-07-06 12:37:32 -0600

    Conditionals

--- CleanArchitecture.ConsoleApp/Program.cs
+++ CleanArchitecture.ConsoleApp/Program.cs
@@ -1,11 +1,37 @@
 ﻿using CleanArchitecture.Data;
 using CleanArchitecture.Domain;
+using Microsoft.EntityFrameworkCore;
 
 StreamerDbContext dbContext = new();
 
 //await AddNewRecords();
+//QueryStreaming();
+await QueryFilter();
 
-QueryStreaming();
+Console.WriteLine("Presione cualquier tecla para terminar el programa");
+Console.ReadKey();
+
+
+async Task QueryFilter()
+{
+    Console.WriteLine($"Ingrese una compania de streaming: ");
+    var streamingNombre = Console.ReadLine();
+
+    var streamers = await dbContext.Streamers.Where(x => x.Nombre.Equals(streamingNombre)).ToListAsync();
+
+    foreach (var streamer in streamers)
+    {
+        Console.WriteLine($"{streamer.Id} - {streamer.Nombre}");
+    }
+
+    //var streamerPartialResults = await dbContext.Streamers.Where(x => x.Nombre.Contains(streamingNombre)).ToListAsync();
+    var streamerPartialResults = await dbContext.Streamers.Where(x => EF.Functions.Like(x.Nombre, $"%{streamingNombre}%")).ToListAsync();
+
+    foreach (var streamer in streamerPartialResults)
+    {
+        Console.WriteLine($"{streamer.Id} - {streamer.Nombre}");
+    }
+}
 
 
 
