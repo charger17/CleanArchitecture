f6d2a0a854776b224ecfb91c2badb8b9b20dd7a7
Carlos Perez Salazar <cps172779@gmail.com>
2023-07-11 13:54:51 -0600

    Add Web Api project

--- CleanArchitecture.Domain/Common/BaseDomainModel.cs
+++ CleanArchitecture.Domain/Common/BaseDomainModel.cs
@@ -1,7 +1,16 @@
 ﻿namespace CleanArchitecture.Domain.Common
 {
-    public class BaseDomainModel
+    public abstract class BaseDomainModel
     {
         public int Id { get; set; }
+
+        public DateTime? DateCreate { get; set; }
+
+        public string? CreatedBy { get; set; }
+
+        public DateTime? LastModifiedDate { get; set; }
+
+        public string? LastModifiedBy { get; set; }
+
     }
 }
