f6b6b411394bde3f519d6f37687fc060c2209a29
Carlos Perez Salazar <cps172779@gmail.com>
2023-07-17 19:01:09 -0600

    Add presentation layer and controllers

---
+++ CleanArchitecture.API/Controllers/VideoController.cs
@@ -0,0 +1,30 @@
+﻿using CleanArchitecture.Application.Features.Videos.Queries.GetVideosList;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace CleanArchitecture.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VideoController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public VideoController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("{username}", Name = "GetVideo")]
+        [ProducesResponseType(typeof(IEnumerable<VideosVm>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<IEnumerable<VideosVm>>> GetVideosByUserName(string username)
+        {
+            var query = new GetVideosListQuery(username);
+
+            var videos = await _mediator.Send(query);
+
+            return Ok(videos);
+        }
+    }
+}
