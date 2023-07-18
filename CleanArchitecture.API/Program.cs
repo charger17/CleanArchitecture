1f7fc23f6ce3afcad9200a04a8904c033776a6e7
Carlos Perez Salazar <cps172779@gmail.com>
2023-07-17 22:09:47 -0600

    Add injection api

--- CleanArchitecture.API/Program.cs
+++ CleanArchitecture.API/Program.cs
@@ -1,3 +1,6 @@
+using CleanArchitecture.Application;
+using CleanArchitecture.Infraestructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//ADD DEPENDENCIES
+builder.Services.AddInfraestructureServices(builder.Configuration);
+builder.Services.AddApplicationServices();
+
 
 var app = builder.Build();
 
