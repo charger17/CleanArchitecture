37844cf056860ffdce0ed55cd5a6a562522420e3
Carlos Perez Salazar <cps172779@gmail.com>
2023-07-20 13:47:16 -0600

    Unit of work around of layers

--- CleanArchitecture.Application/Contracts/Persistence/IUnitOfWork.cs
+++ CleanArchitecture.Application/Contracts/Persistence/IUnitOfWork.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CleanArchitecture.Domain.Common;
 
 namespace CleanArchitecture.Application.Contracts.Persistence
 {
-    internal class IUnitOfWork
+    public interface IUnitOfWork :IDisposable
     {
+        IAsyncRepository<TEntity> Repository<TEntity>() where TEntity : BaseDomainModel;
+
+        Task<int> Complete();
     }
 }
