117d516fdbaefca85ba096693c16ce82380777f1
Carlos Perez Salazar <cps172779@gmail.com>
2023-07-20 16:01:23 -0600

    Unit of work for Custom Repositories

--- CleanArchitecture.Application/Contracts/Persistence/IUnitOfWork.cs
+++ CleanArchitecture.Application/Contracts/Persistence/IUnitOfWork.cs
@@ -2,8 +2,11 @@
 
 namespace CleanArchitecture.Application.Contracts.Persistence
 {
-    public interface IUnitOfWork :IDisposable
+    public interface IUnitOfWork : IDisposable
     {
+        IStreamerRepository StreamerRepository { get; }
+        IVideoRepository VideoRepository { get; }
+
         IAsyncRepository<TEntity> Repository<TEntity>() where TEntity : BaseDomainModel;
 
         Task<int> Complete();
